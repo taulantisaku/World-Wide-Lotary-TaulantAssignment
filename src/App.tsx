cec1e74a5d1ab42c22bd5c476aa8e9e8f18f8a5f
Taulant-kutia <44265863+taulantisaku@users.noreply.github.com>
2022-07-16 20:57:26 +0200

    Create homepage and User card

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Home } from "./components/Home/Home";
 import { SessionPlayers } from "./components/SessionPlayers/SessionPlayers";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <Home />
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,68 @@
-import React from "react";
-
+import axios from "axios";
 import "../Home/Home.scss";
+import { useQuery } from "react-query";
+import User from "../User/User";
+
+// export interface HomeType {
+//   enabled: boolean;
+// }
 
 export const Home = () => {
-  return <div>Home</div>;
+  const { isLoading, isError, data, refetch } = useQuery(
+    ["query-users"],
+    async () => {
+      const response = await axios.get(
+        `https://randomuser.me/api?page={pageIndex}`
+      );
+      const user = await response.data.results[0];
+      const userAge = response.data.results[0].dob.age;
+      const ranNum = Math.floor(Math.random() * 100);
+      // console.log(user);
+      // console.log("Name: ", user.name.first);
+      // console.log("userAge", userAge);
+      // console.log("ranNum", ranNum);
+      if (ranNum === userAge) {
+        alert("THE WINNER IS: " + user.name.first);
+      }
+
+      return user;
+    }
+    // {
+    //   enabled: false,
+    // }
+  );
+
+  const handleClick = () => {
+    refetch();
+  };
+
+  return (
+    <div className="Home">
+      <div className="Home__header">
+        <ul className="Home__header__navlist">
+          <li>Home</li>
+          <li>Winners</li>
+          <li>Session Players</li>
+          <li>Statistics</li>
+        </ul>
+      </div>
+      <div className="Home__ButtonSection">
+        <button
+          onClick={handleClick}
+          className="Home__ButtonSection__GenerateButton"
+        >
+          Generate user
+        </button>
+      </div>
+      {data && (
+        <>
+          <div className="Home__User">
+            <User user={data} />
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Home;
